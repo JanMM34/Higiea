e33af679d602a3a091763d802bfe8a2138ade93b
JanMM34 <janmanelmorales@gmail.com>
2024-11-12 12:35:22 +0100

    Added Route Entity

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS sensor;
 DROP TYPE IF EXISTS container_state;
+DROP TABLE IF EXISTS truck;
+DROP TABLE IF EXISTS route;
 
 CREATE TYPE container_state AS ENUM ('FULL', 'EMPTY');
 
@@ -8,4 +10,8 @@ CREATE TABLE sensor (
     latitude DOUBLE PRECISION NOT NULL,
     longitude DOUBLE PRECISION NOT NULL,
     state container_state NOT NULL
+);
+
+CREATE TABLE truck (
+    id SERIAL PRIMARY KEY
 );
